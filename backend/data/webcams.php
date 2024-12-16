44ffa9f8c8b80fe6313ce638b19cbbad345940b6
jgray-dev <nohaxjustdoge@gmail.com>
2024-12-15 20:42:08 -0500

    Add files via upload

--- backend/data/webcams.php
+++ backend/data/webcams.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
-$jsonData = file_get_contents('changelog.json');
+$jsonData = file_get_contents('webcams.json');
 $jsonArray = json_decode($jsonData, true);
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo $jsonData;
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $postData = json_decode(file_get_contents('php://input'), true);
     $jsonArray[] = $postData;
     $jsonData = json_encode($jsonArray);
-    file_put_contents('changelog.json', $jsonData);
+    file_put_contents('webcams.json', $jsonData);
     http_response_code(201);
     echo json_encode(array("message" => "Data added successfully"));
 }
